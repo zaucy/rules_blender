a4da44294a9e0dab876898c4b4dd0c3fbc299638
Ezekiel Warren <zaucy@users.noreply.github.com>
2022-01-16 22:52:25 -0800

    Validating inputs in blender renders (#14)

---
+++ rules_blender_scripts/bazel_check_linked.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import bpy
+import argparse
+
+blender_render_inputs = os.environ["BAZEL_BLENDER_RENDER_INPUTS"]
+
+if not blender_render_inputs:
+  sys.exit("Missing environment variable 'BAZEL_BLENDER_RENDER_INPUTS'")
+
+blend_paths = bpy.utils.blend_paths(absolute = True)
+inputs = []
+
+with open(blender_render_inputs) as file:
+  while (line := file.readline().rstrip()):
+    inputs.append(line)
+    inputs.append(os.path.abspath(line))
+
+for blend_path in blend_paths:
+  if not (blend_path in inputs):
+    sys.exit(
+      "Cannot find '{}'. Make sure '{}' is declared in a blender_library srcs in blender_render deps.".format(
+        blend_path,
+        os.path.basename(blend_path),
+      ),
+    )
